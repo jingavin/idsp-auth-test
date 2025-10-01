501aaff9aa957bafd4c48a48452566aa08f36281
Gavin <gavinjin62@gmail.com>
2025-09-30 23:07:29 -0700

    add boiler plate clerk auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Slot } from 'expo-router';
+
+const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
+import { SignOutButton } from "@/components/SignOutButton";
+import { SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+    <SignedIn>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>logged in page here</Text>
+        <SignOutButton />
+      </View>
+    </SignedIn>
+
+
+<SignedOut>
+  <Redirect href="/(auth)/sign-in" />
+</SignedOut>
+    </>
   );
 }
