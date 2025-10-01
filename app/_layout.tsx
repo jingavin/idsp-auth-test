1e5f04a2cb6c5bcbad0014cc17bf80435bf4b80d
Gavin <gavinjin62@gmail.com>
2025-09-30 21:42:30 -0700

    add clerkprovider package and wrapper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ClerkProvider } from '@clerk/clerk-expo';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,6 +13,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <ClerkProvider>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -20,5 +21,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </ClerkProvider>
   );
 }
